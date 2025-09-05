b0ce20d81bd8e881245fa0162445214e6c644aa7
rob587 <robertocamm02@yahoo.com>
2025-09-05 14:59:59 +0200

    components and imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Homepage from "./pages/Homepage"
 import About from "./pages/About"
 import Products from "./pages/Products"
 
+import  DefaultLayout from "./layout/DefaultLayout"
+import  {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   
