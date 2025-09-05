5b6bb2828ad6cfb7144f84d35c9640699222b2cf
rob587 <robertocamm02@yahoo.com>
2025-09-05 15:03:48 +0200

    BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,20 @@ function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route element = {<DefaultLayout/>}>
+            <Route path="/" element={<Homepage/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/products" element={<Products/>}></Route>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+
+
+
     </>
   )
 }
