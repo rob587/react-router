613a95afe2531c409d639112fb95a0d63e5df26d
rob587 <robertocamm02@yahoo.com>
2025-09-05 22:38:32 +0200

    prodotti renderizzati

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const Products = () => {
+  
+  const [products, setProducts] = useState([])
+  
+  const url = ("https://fakestoreapi.com/products")
+  
+  const fetchData = () =>{
+    axios.get(url).then((resp)=>{
+      setProducts(resp.data)
+    })
+  }
+
+    useEffect(() => {
+    fetchData()
+  }, [])
+
+
   return (
     <div>
-      {/* da sviluppare */}
+      <div className="container">
+        <div className="row">
+
+            {products.map((product)=>{
+              return(
+            <div className="col-4" key={product.id}>
+              <img src={product.image} alt={product.description} />
+              <h2>{product.title}</h2>
+              <p>{product.price}</p>
+           </div>
+
+            )
+
+              
+            })}
+
+        </div>
+      </div>
     </div>
   )
 }
