ef37ca728f897a91f71f0748973892cfe93469ab
rob587 <robertocamm02@yahoo.com>
2025-09-05 23:37:00 +0200

    added style

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -21,18 +21,18 @@ const Products = () => {
 
   return (
     <div>
-      <div className="container">
-        <div className="row">
+      <div className="container mt-5">
+        <div className="row g-4">
 
             {products.map((product)=>{
               return(
-            <div className="col-4 mt-5" key={product.id}>
-             <div className="card">
-                <img src={product.image}  alt={product.description}/>
-                <div className="card-body">
-                  <h2 className="card-title">{product.title}</h2>
+            <div className="col-4 d-flex mt-5" key={product.id}>
+             <div className="card shadow-sm h-100">
+                <img className="card-img-top p-3" src={product.image}  alt={product.description}/>
+                <div className="card-body ">
+                  <h2 className="card-title ">{product.title}</h2>
                   <p className="card-text">{product.description}</p>
-                  <h3 className="card-text">{product.price}</h3>
+                  <h3 className="card-text mt-auto fw-bold">{product.price}$</h3>
                  
                 </div>
               </div>
