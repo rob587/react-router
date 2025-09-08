070762ad965a7acefc47d584ae7e0f519550f97a
rob587 <robertocamm02@yahoo.com>
2025-09-08 16:06:31 +0200

    [change] css

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -23,12 +23,12 @@ const DetailProduct = () => {
 
 
   return (
-    <div className="container">
-        <div className="row">
+    <div className="container mt-5">
+        <div className="row d-flex justify-content-center align-items-center">
             {newDetails.map((detail)=>{
                 return (
-                <div className="col-12" key={detail.id}>
-                    <div className="card shadow-sm h-100">
+                <div className="col-6" key={detail.id}>
+                    <div className="card shadow-lg h-100 ">
                 <img className="card-img-top p-3" src={detail.image}  alt={detail.description}/>
                 <div className="card-body ">
                   <h2 className="card-title ">{detail.title}</h2>
