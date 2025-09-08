7e97dde98db7625a0a07f18210ccb21bf117d81f
rob587 <robertocamm02@yahoo.com>
2025-09-08 15:37:15 +0200

    detailproduct

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 
 const DetailProduct = () => {
+    
+    const [newDetails, setNewDetails] = useState([])
+    
+    const newUrl = "https://fakestoreapi.com/products/:id"
+    
+    
+    
+    const fetchDetail = () =>{
+        axios.get(newUrl).then((resp)=>{
+          setNewDetails([resp.data])
+        })
+      }
+
+        useEffect(() => {
+          fetchDetail()
+        }, [])
+
+
+
   return (
     <div className="container">
         <div className="row">
-            <div className="col-12">
-                <h1>ciao</h1>
+            {newDetails.map((detail)=>{
+                return (
+                <div className="col-12" key={detail.id}>
+                    <div className="card shadow-sm h-100">
+                <img className="card-img-top p-3" src={detail.image}  alt={detail.description}/>
+                <div className="card-body ">
+                  <h2 className="card-title ">{detail.title}</h2>
+                  <p className="card-text">{detail.description}</p>
+                  <h3 className="card-text mt-auto fw-bold">{detail.price}$</h3>
+                 
+                </div>
+              </div>
             </div>
+
+                )
+            })}
         </div>
     </div>
   )
